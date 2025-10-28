43eb605ab5039998cb1b2ef3f76c6f24f795dbd8
vps <vps>
2025-10-28 13:35:56 +0000

    vps fix

--- out/assets/Card-119e7607.js
+++
@@ -1,2 +0,0 @@
-import{j as o}from"./index-11de833d.js";function g({children:r,className:d="",padding:s="md"}){const a={sm:"p-3",md:"p-4",lg:"p-6"};return o.jsx("div",{className:`bg-white dark:bg-gray-800 rounded-lg shadow-sm border border-gray-200 dark:border-gray-700 ${a[s]} ${d}`,children:r})}export{g as C};
-//# sourceMappingURL=Card-119e7607.js.map
